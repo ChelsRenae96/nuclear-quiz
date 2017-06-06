e06330503d8b7b5017e17dd2e152b9f1ecd3bc3d
ChelsRenae96 <ccarr24@vols.utk.edu>
2017-06-06 15:43:21 -0400

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,3 +1,4 @@
+import time
 Y(0)=5
 Y(t)=-2t
 def rk4(f, t0, y0, t1, n):
@@ -20,3 +21,4 @@ vt, vy = rk4(f, 0, 5, 10, 100)
 for t, y in list(zip(vt, vy))[::10]:
     print ("%4.1f %10.5f %+12.4e" % (t, y, y - (t**2 +5))
     # the "%4.1...12.4e" part is something i found on many other examples for placement, but I am unsure of its purpose. Help?
+time.sleep(100)
