58f38c9829c54209ef8df32ed197c80964b8b1a6
ChelsRenae96 <ccarr24@vols.utk.edu>
2017-06-08 19:46:54 -0400

    Create test_1.py

--- test_1.py
+++ test_1.py
@@ -1,11 +1,11 @@
 Y(0)=5
-Y(t)=-2t
 def rk4(f, t0, y0, vy, n):
     vt = [0] * (n + 1)
     vy = [0] * (n + 1)
     h = (t1 - t0) / float(n)
     t0=vt[0] = t
     y0=vy[0] = y
+    Y(t)=-2t
     for i in range(1, n + 1):
         k1 = h * f(t, y)
         k2 = h * f(t + 0.5 * h, y + 0.5 * k1)
